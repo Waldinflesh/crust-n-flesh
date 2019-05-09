d112077b919876d68f5c35d4c7b70710260c049f
Waldinflesh <antonelli.1802985@studenti.uniroma1.it>
2019-05-09 13:33:03 +0200

    modified packages structure

---
+++ src/main/kotlin/crustnflesh/crustnflesh.kt
@@ -0,0 +1,12 @@
+package crustnflesh
+
+import net.minecraftforge.fml.common.Mod
+
+const val modid = "crustnflesh"
+const val name = "Crust n' Flesh"
+const val version = "0.01F"
+
+@Mod(modid = modid, name = name, version = version, modLanguageAdapter = "net.shadowfacts.forgelin.KotlinAdapter", acceptedMinecraftVersions = "1.12.2")
+object CrustNFleshMod {
+
+}
