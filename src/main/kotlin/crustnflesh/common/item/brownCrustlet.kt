7e4f92ee1960b51624b4c33c6cdc8d7bfabb63c8
Waldinflesh <antonelli.1802985@studenti.uniroma1.it>
2019-05-10 20:50:19 +0200

    Added item: Brown crustlet

---
+++ src/main/kotlin/crustnflesh/common/item/brownCrustlet.kt
@@ -0,0 +1,28 @@
+package crustnflesh.common.item
+
+import crustnflesh.modid
+import net.minecraft.creativetab.CreativeTabs
+import net.minecraft.item.Item
+import net.minecraft.util.ResourceLocation
+import net.minecraftforge.event.RegistryEvent
+import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+
+
+val brownCrustlet:Item = object:Item(){
+    init{
+        unlocalizedName = "Brown crustlet"
+        registryName = ResourceLocation(modid, "brown_crustlet")
+        setCreativeTab(CreativeTabs.MISC)
+    }
+
+
+}
+@Mod.EventBusSubscriber(modid=modid)
+object RegistryHandler{
+    @JvmStatic
+    @SubscribeEvent
+    fun onItemRegister(event: RegistryEvent.Register<Item>){
+        event.getRegistry().register(brownCrustlet)
+    }
+}
