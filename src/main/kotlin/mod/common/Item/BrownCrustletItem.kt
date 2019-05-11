099e507232b224f0232f310c09a25959bf5e3550
Matteo Semenzato <mattew8898@gmail.com>
2019-05-11 18:55:22 +0200

    Added Brown Crustlet texture.

--- src/main/kotlin/mod/common/Item/BrownCrustletItem.kt
+++ src/main/kotlin/mod/common/Item/BrownCrustletItem.kt
@@ -7,12 +7,18 @@ import net.minecraft.util.ResourceLocation
 import net.minecraftforge.event.RegistryEvent
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import net.minecraftforge.client.model.ModelLoader
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraftforge.client.event.ModelRegistryEvent
+
+
+
 
 
 val brownCrustlet:Item = object:Item(){
     init{
         setUnlocalizedName("BrownCrustlet")
-        setRegistryName("BrownCrustlet")
+        setRegistryName("browncrustlet")
         setCreativeTab(CreativeTabs.MISC)
     }
 
@@ -25,4 +31,12 @@ object RegistryHandler{
     fun onItemRegister(event: RegistryEvent.Register<Item>){
         event.getRegistry().register(brownCrustlet)
     }
+    @JvmStatic
+    @SubscribeEvent
+    fun registerModels(event: ModelRegistryEvent) {
+        // ModelLoader.setCustomModelResourceLocation(TutorialItem, 0, ModelResourceLocation(TutorialItem.registryName ?: return, "inventory"))
+        ModelLoader.setCustomModelResourceLocation(brownCrustlet, 0, ModelResourceLocation("crustnflesh:browncrustlet", "inventory"))
+    }
+
+
 }
--- src/main/kotlin/mod/crustnflesh.kt
+++ src/main/kotlin/mod/crustnflesh.kt
@@ -8,5 +8,4 @@ const val version = "0.01F"
 
 @Mod(modid = modid, name = name, version = version, modLanguageAdapter = "net.shadowfacts.forgelin.KotlinAdapter", acceptedMinecraftVersions = "1.12.2")
 object CrustNFleshMod {
-
 }
