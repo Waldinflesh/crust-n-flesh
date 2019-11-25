01695fc68f9c4bf88afab9f5c7483899d56bf9bc
Waldinflesh <antonelli.1802985@studenti.uniroma1.it>
2019-11-25 11:15:59 +0100

    merge branch 'fleshgrater'

--- src/main/kotlin/mod/client/gui/GuiNetController.kt
+++ src/main/kotlin/mod/client/gui/GuiNetController.kt
@@ -26,11 +26,6 @@ class GuiNetController(container: Container, playerInv: InventoryPlayer): GuiCon
     //GUI Buttons
     val RESCAN = 0
 
-    init {
-        this.playerInv = playerInv
-        this.container = container as ContainerNetController
-    }
-
     override fun initGui() {
         super.initGui()
 
